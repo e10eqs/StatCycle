8c8eb1a38f6c8b3b73de5fa6c8ec4ce12454a511
Brandon Lusk <blusk1128@gmail.com>
2022-12-11 16:09:00 -0600

    finished

--- Modus/Free_RTOS_Final_Project/task_dummy_velcoity.c
+++ Modus/Free_RTOS_Final_Project/task_dummy_velcoity.c
@@ -5,13 +5,9 @@ void Task_Dummy_Velocity(void *pvParameters) {
 	enum pvtState_e state = NO_FIX;
 	Display d = { NUM, { 0, 0, 0, 0 } };
 	enum states new_display_state = NUM;
-	static int data = 0;
 
 	while (1) {
 		state = getPVT();
-		data += 5;
-		data = data % 255;
-		xQueueSend(Queue_Speed, &data, portMAX_DELAY);
 		switch (state) {
 		case NO_FIX:
 			new_display_state = FLASH;
@@ -25,18 +21,21 @@ void Task_Dummy_Velocity(void *pvParameters) {
 				Fil = malloc(sizeof(FIL));
 				int sz = sizeof(char) * 200;
 				char* file_name = malloc(sz);
-				snprintf(file_name, sz, "%d%d-%02d%02d%d.txt", pvt.hour, pvt.min, pvt.month, pvt.day, pvt.year);
+				snprintf(file_name, sz, "%d-%02d-%02dT%02d-%02d.txt", pvt.year, pvt.month, pvt.day, pvt.hour, pvt.min);
 				fr = f_open(Fil, file_name, FA_CREATE_ALWAYS | FA_WRITE);
  				free(file_name);
 			}
 			double speed = pvt.groundSpeed / 447.04;
-			f_printf(Fil, "Hour: %d, Minute: %d, Second: %d Latitude: %ld Longitude: %ld, SPEED: %ld\r\n", pvt.hour, pvt.min, pvt.sec, pvt.latitude, pvt.longitude, speed);
+			int print_speed = speed * 100;
+			int radio_speed = speed;
+			f_printf(Fil, "%d-%02d-%02dT%02d:%02d:%02d Latitude: %lf Longitude: %lf, SPEED: %.2lf\r\n", pvt.year, pvt.month, pvt.day, pvt.hour, pvt.min, pvt.sec, pvt.latitude /10000000.0, pvt.longitude/10000000.0, speed);
 			d.current_state = NUM;
 			for (int i = 3; i >= 0; i--) {
-				d.display[i] = fmod(speed, 10);
-				speed /= 10;
+				d.display[i] = fmod(print_speed, 10);
+				print_speed /= 10;
 			}
 			xQueueSend(Queue_Display, &d, portMAX_DELAY);
+			xQueueSend(Queue_Speed, &radio_speed, portMAX_DELAY);
 
 			break;
 		case POLL_ERROR:
